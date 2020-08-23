102bf6944a00aa0dc68c19d9cacf69be1229e30c
kauaas <57207681+kauaas@users.noreply.github.com>
2020-08-23 14:59:14 -0300

    Add files via upload

---
+++ ATIV05.py
@@ -0,0 +1,10 @@
+N1 = float (input('Informe um número: '))
+N2 = float (input('Informe um número: '))
+N3 = float (input('Informe um número: '))
+
+if (N1>N2) and (N1>N3):
+    print('O maior número é {} '.format(n1))
+elif (N2>N1) and (N2>N3):
+    print('O maior número é {} '.format(N2))
+else:
+    print('O maior número é {} '.format(N3))
