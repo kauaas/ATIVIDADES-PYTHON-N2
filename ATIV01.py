0e67bc0417eb1bd364d1ac06b15ea3bc34952bbc
kauaas <57207681+kauaas@users.noreply.github.com>
2020-08-23 14:51:44 -0300

    Add files via upload

---
+++ ATIV01.py
@@ -0,0 +1,7 @@
+n1 = float(input('Informe um número: '))
+n2 = float(input('Informe um número: '))
+
+if (n1>n2):
+    print('O maior número detectado foi {}'.format(n1))
+else:
+    print('O menor detectado número foi {}'.format(n2))
