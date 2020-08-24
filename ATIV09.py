cfb183f0f7d3527a35a76f9aa5ebcc318889f755
kauaas <57207681+kauaas@users.noreply.github.com>
2020-08-24 20:57:54 -0300

    Add files via upload

---
+++ ATIV09.py
@@ -0,0 +1,10 @@
+Turno = input('Informe o turno em que você estuda: ')
+
+if (Turno=='m') or (Turno=='M'):
+    print('BOM DIA!')
+elif(Turno=='v' or (Turno=='F')):
+    print('BOA TARDE!')
+elif(Turno=='n') or (Turno=='N'):
+    print('BOA NOITE!')
+else:
+    print('Turno Inválido!')
